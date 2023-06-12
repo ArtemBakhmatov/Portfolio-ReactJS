68cce62c2ef17f804f529eccc6b62278bd6a7b51
artem_bakhmatov <ip.bakhmatov@mail.ru>
2023-06-12 17:10:31 +0300

    Карточка с проектом, компонент Project

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
 	return (
 		<div className="App">
 			<Navbar />
-			{/* <Projects /> */}
+			<Projects />
 			{/* <Home /> */}
-			<Contacts />
+			{/* <Contacts /> */}
 			<Footer />
 		</div>
 	);
